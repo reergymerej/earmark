705035777ffe7875bf0a25e13b95d8e9bb98bfbb
RobertDober <robert.dober@gmail.com>
2019-12-08 17:27:58 +0100

    missspec corrected

--- test/acceptance/ast/gfm-lists/list_test.exs
+++ test/acceptance/ast/gfm-lists/list_test.exs
@@ -2,8 +2,27 @@ defmodule Acceptance.Ast.GfmLists.ListTest do
   use ExUnit.Case
   import Support.AstHelpers
 
-  @moduletag :wip
 
+  describe "What are lists?" do
+    test "GFM Spec #231" do
+      markdown = """
+      A paragraph
+      with two lines.
+
+          indented code
+
+      > A block quote.
+      """
+      expected = [
+        {"p", [], ["A paragraph\nwith two lines."]},
+        {"pre", [], [{"code", [], ["indented code"]}]},
+        {"blockquote", [], [{"p", [], ["A block quote."]}]}
+      ]
+
+
+      assert ast_from_md(markdown) == expected
+    end
+  end
   describe "Lists can interrupt paragraphs" do
     test "GFM Spec #283" do
       markdown = """
@@ -11,7 +30,7 @@ defmodule Acceptance.Ast.GfmLists.ListTest do
       - bar
       - baz
       """
-      expected = [{"p", [], ["Foo", {"ul", [], [{"li", [], ["bar"]}, {"li", [], ["baz"]}]}]}]
+      expected = [{"p", [], ["Foo"]}, {"ul", [], [{"li", [], ["bar"]}, {"li", [], ["baz"]}]}]
 
       assert ast_from_md(markdown) == expected
     end
